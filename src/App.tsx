14b549fdf7051e2a72b59fd8ff474eb53a8ecfac
perfumelim <perfumelim@gmail.com>
2022-02-25 16:26:00 +0900

    feat: route 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import DetailPage from "pages/DetailPage";
 import LinkPage from "pages/LinkPage";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Path } from "utils/constants";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Container>
-        <LinkPage />
-        {/* <DetailPage /> */}
+        <Router>
+          <Routes>
+            <Route path={Path.Home} element={<LinkPage />} />
+            <Route path={Path.Detail} element={<DetailPage />} />
+          </Routes>
+        </Router>
       </Container>
     </>
   );
