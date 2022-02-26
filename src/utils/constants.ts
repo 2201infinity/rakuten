b1369d61a303dc16c88542d356ed8520240b7191
perfumelim <perfumelim@gmail.com>
2022-02-26 16:51:26 +0900

    refactor: proxy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,6 +8,8 @@ export const today = 1641890000;
 export const wrongImagePath =
   "https://storage-fe.fastraffic.io/homeworks/thumbnails/static/pdf.svg";
 
-export const API_URL = "/homeworks/links";
+export const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+
+export const API_URL = `${PROXY}/homeworks/links`;
 
 export const baseUrl = "http://localhost:3001/";
